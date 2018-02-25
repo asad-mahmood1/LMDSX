d76a34ccd0e9abf50fcbb67687d24585a26cd5ab
cerebralace <30564031+cerebralace@users.noreply.github.com>
2018-02-25 16:35:16 -0500

    Add files via upload

---
+++ Lab 2/connection.R
@@ -0,0 +1,12 @@
+# connection.R
+
+# Fill in connection details about your DB2 Warehouse instance below.
+
+dsn.hostname <- 'dashdb-entry-yp-dal09-07.services.dal.bluemix.net'
+dsn.uid <- 'dash10135'
+dsn.pwd <- 'Tgl4_RiQA8_h'
+dsn.database <- 'BLUDB'
+dsn.port <- '50000'
+dsn.protocol <- 'TCPIP'
+
+vetting.table = 'SUPPLYCHAIN'
